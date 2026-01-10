d5ecb21be2c02346fca87fd3095f1931110bb2ae
Matthieu Turbier <matthieu@air-de-matthieu.lan>
2026-01-10 17:43:07 +0100

    Move GA4 to _app and clean _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,44 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
-import React from "react";
-import { PermissionProvider } from "../components/PermissionProvider";
-import "../styles/globals.css";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "G-0J8NXZ3SBD"; // tu peux enlever le fallback après
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag?.("config", GA_ID, { page_path: url });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return (
-    <PermissionProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { anonymize_ip: true });
+        `}
+      </Script>
+
       <Component {...pageProps} />
-    </PermissionProvider>
+    </>
   );
 }
