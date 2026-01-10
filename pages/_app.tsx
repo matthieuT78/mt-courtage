3cb9efa88880387ff94dcfe6a9c781eba41284f9
Matthieu Turbier <matthieu@air-de-matthieu.lan>
2026-01-10 17:05:16 +0100

    remove next sitemap route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,35 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
 import React from "react";
+import Script from "next/script";
 import { PermissionProvider } from "../components/PermissionProvider";
 
-// ✅ IMPORTANT : réactive le CSS global (Tailwind ou global CSS)
+// CSS global (Tailwind / global styles)
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PermissionProvider>
-      <Component {...pageProps} />
-    </PermissionProvider>
+    <>
+      {/* Google Analytics GA4 */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0J8NXZ3SBD"
+      />
+
+      <Script id="ga4" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0J8NXZ3SBD', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <PermissionProvider>
+        <Component {...pageProps} />
+      </PermissionProvider>
+    </>
   );
 }
