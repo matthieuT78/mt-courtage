626cb20cf610825f79c024165c775915778fa531
Matthieu Turbier <matthieu@air-de-matthieu.lan>
2026-01-11 22:46:03 +0100

    SEO: pages dynamiques + sitemap auto + favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
-      <body>
+      <Head>
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Couleur navigateur / mobile */}
+        <meta name="theme-color" content="#0f172a" />
+
+        {/* Identité app */}
+        <meta name="application-name" content="lokt.fr" />
+        <meta name="apple-mobile-web-app-title" content="lokt.fr" />
+      </Head>
+      <body className="bg-slate-100">
         <Main />
         <NextScript />
       </body>
