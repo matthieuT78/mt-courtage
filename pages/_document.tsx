e7c21f888ea427d16121528b99540a8e4374a389
Matthieu Turbier <matthieu@air-de-matthieu.lan>
2026-01-15 17:10:10 +0100

    Fix Google-compatible favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         {/* Favicons */}
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
