c727acfcc9e62e67594c9f586b029716a4fd7c54
Matthieu Turbier <matthieu@air-de-matthieu.lan>
2026-01-17 11:55:34 +0100

    Fix: espace mobile sur le bandeau 'Fonctionnalité à venir' (index)

--- pages/index.tsx
+++ pages/index.tsx
@@ -566,10 +566,11 @@ export default function Home() {
 
                   <div className="space-y-2">
                     <h3 className="text-2xl sm:text-3xl font-semibold leading-tight">
-                      Votre gestion locative, au même endroit
-                      <br className="hidden sm:block" />
-                      que vos simulations.
-                    </h3>
+                    Votre gestion locative, au même{" "}
+		    endroit
+		   <br className="hidden sm:block" />
+		   que vos simulations.
+		    </h3>
 
                     <p className="text-sm text-white/90 max-w-2xl">
                       L’espace bailleur lokt.fr est en préparation : un endroit clair pour{" "}
